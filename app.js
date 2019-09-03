980518c5914ffbff2ab98e7153f9c77b56efaef5
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2019-09-03 17:48:08 -0300

    adding all the notes app functionality

--- app.js
+++ app.js
@@ -1,8 +1,56 @@
+const yargs = require("yargs");
 const notes = require("./notes");
-const chalk = require("chalk");
 
 const log = console.log;
 
-log(notes.getNotes());
-log(chalk.green("Success!"));
-log(chalk.green.inverse.bold("Success!"));
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title to add",
+      demandOption: true,
+      type: "string"
+    },
+    body: {
+      describe: "Note content body to add",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: argv => notes.addNote(argv.title, argv.body)
+});
+
+yargs.command({
+  command: "remove",
+  describe: "Removes an existing note",
+  builder: {
+    title: {
+      describe: "Note title to remove",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: argv => notes.removeNote(argv.title)
+});
+
+yargs.command({
+  command: "list",
+  describe: "List all added notes",
+  handler: () => notes.listNotes()
+});
+
+yargs.command({
+  command: "read",
+  describe: "Read a particular note",
+  builder: {
+    title: {
+      describe: "Note title to read",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: argv => notes.readNote(argv.title)
+});
+
+yargs.parse();
--- notes.js
+++ notes.js
@@ -1,3 +1,83 @@
-const getNotes = () => "Your notes...";
+const fs = require("fs");
+const chalk = require("chalk");
 
-module.exports = { getNotes };
+const log = console.log;
+
+const readNote = title => {
+  const notes = loadNotes();
+
+  const note = notes.find(note => note.title === title);
+
+  if (note) {
+    log(chalk.blue(note.title + ": ") + note.body);
+  } else {
+    log(chalk.red.inverse("Note does not exist"));
+  }
+};
+
+const addNote = (title, body) => {
+  const notes = loadNotes();
+
+  if (doesNoteWithTitleExists(notes, title)) {
+    notes.push({
+      title,
+      body
+    });
+    saveNotes(notes);
+    log(chalk.green.inverse("New note added!"));
+  } else {
+    log(chalk.red.inverse("Note already exists!"));
+  }
+};
+
+const removeNote = title => {
+  const notes = loadNotes();
+
+  if (!doesNoteWithTitleExists(notes, title)) {
+    notesToKeep = notes.filter(note => note.title !== title);
+    saveNotes(notesToKeep);
+    log(chalk.green.inverse(`Note with title \"${title}\" removed.`));
+  } else {
+    log(chalk.red.inverse("Note does not exists!"));
+  }
+};
+
+const listNotes = () => {
+  const notes = loadNotes();
+
+  log(chalk.blue("Your notes"));
+
+  notes.forEach(note => {
+    log(note.title);
+  });
+};
+
+const saveNotes = notes => {
+  const jsonData = JSON.stringify(notes);
+  fs.writeFileSync("./notes.json", jsonData);
+};
+
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync("./notes.json");
+    const jsonData = dataBuffer.toString();
+    return JSON.parse(jsonData);
+  } catch (error) {
+    return [];
+  }
+};
+
+const doesNoteWithTitleExists = (notes, title) => {
+  const alreadyExistingNotes = notes.find(note => note.title === title);
+  if (!alreadyExistingNotes) {
+    return true;
+  }
+  return false;
+};
+
+module.exports = {
+  readNote: readNote,
+  addNote: addNote,
+  removeNote: removeNote,
+  listNotes: listNotes
+};
